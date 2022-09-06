b9185c02050ca5c24faa2432b781dc706a4361b2
Christian Dressler <dressler@demos-deutschland.de>
2022-09-06 08:57:20 +0200

    feature: add usable phpunit test setup

---
+++ bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+ini_set('error_reporting', E_ALL); // or error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
+require /*dirname(__DIR__) . DIRECTORY_SEPARATOR . */'vendor/autoload.php';
--- packages/queries/tests/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
