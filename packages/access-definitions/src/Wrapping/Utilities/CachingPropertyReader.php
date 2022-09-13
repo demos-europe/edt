b9bfda5c31dfe5a8418e248800794b3a6a376738
Christian Dressler <dressler@demos-deutschland.de>
2022-09-13 16:34:51 +0200

    feature: add `CachingPropertyReader`

---
+++ packages/access-definitions/src/Wrapping/Utilities/CachingPropertyReader.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EDT\Wrapping\Utilities;
+
+use EDT\Querying\Utilities\Iterables;
+use EDT\Wrapping\Contracts\WrapperFactoryInterface;
+use InvalidArgumentException;
+use function array_key_exists;
+use EDT\Wrapping\Contracts\Types\ReadableTypeInterface;
+use function gettype;
+use function is_object;
+
+class CachingPropertyReader extends PropertyReader
+{
+    /**
+     * @var array<non-empty-string, mixed>
+     */
+    private $valueCache = [];
+
+    protected function determineRelationshipValue(
+        WrapperFactoryInterface $wrapperFactory,
+        ReadableTypeInterface $relationship,
+        $propertyValue
+    ) {
+        $hash = $this->createHash($wrapperFactory, $relationship, $propertyValue);
+        if (!array_key_exists($hash, $this->valueCache)) {
+            $value = parent::determineRelationshipValue($wrapperFactory, $relationship, $propertyValue);
+            $this->valueCache[$hash] = $value;
+        }
+
+        return $this->valueCache[$hash];
+    }
+
+    /**
+     * Create cache hash from 3 values that need to be distinct to be cached.
+     *
+     * @param mixed|null $propertyValue
+     *
+     * @return non-empty-string
+     */
+    private function createHash(WrapperFactoryInterface $wrapperFactory, ReadableTypeInterface $relationship, $propertyValue): string
+    {
+        $hashRelationship = spl_object_hash($relationship);
+
+        if (null === $propertyValue) {
+            $hashProperty = '';
+        } elseif (is_scalar($propertyValue)) {
+            // int, float, string or bool
+            $hashProperty = (string) $propertyValue;
+        } elseif (is_object($propertyValue)) {
+            $hashProperty = spl_object_hash($propertyValue);
+        } elseif (is_iterable($propertyValue)) {
+            $hashProperty = serialize(Iterables::asArray($propertyValue));
+        } else {
+            $valueType = gettype($propertyValue);
+            throw new InvalidArgumentException("Unexpected value type '$valueType'.");
+        }
+
+        $hashWrapper = spl_object_hash((object) $wrapperFactory);
+
+        return hash('sha256', $hashRelationship.$hashProperty.$hashWrapper);
+    }
+}
