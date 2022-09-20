4bc72357612fd9b06ad614b332fb3ea17bc58a7c
Christian Dressler <dressler@demos-deutschland.de>
2022-09-20 14:07:40 +0200

    prepare release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## 0.12.11 - 2022-09-20
 
 - feature: allow validation of external read-paths
 - feature: validate paths on alias processing
--- packages/access-definitions/tests/Wrapping/Utilities/SchemaPathProcessorTest.php
+++ packages/access-definitions/tests/Wrapping/Utilities/SchemaPathProcessorTest.php
@@ -5,11 +5,17 @@ declare(strict_types=1);
 namespace Tests\Wrapping\Utilities;
 
 use EDT\Querying\ConditionFactories\PhpConditionFactory;
+use EDT\Querying\Contracts\PropertyPathAccessInterface;
+use EDT\Querying\Functions\Property;
+use EDT\Querying\PropertyPaths\PropertyPath;
 use EDT\Querying\SortMethodFactories\PhpSortMethodFactory;
+use EDT\Querying\SortMethods\Ascending;
 use EDT\Wrapping\Contracts\AccessException;
 use EDT\Wrapping\TypeProviders\PrefilledTypeProvider;
 use EDT\Wrapping\Utilities\SchemaPathProcessor;
+use InvalidArgumentException;
 use Tests\data\Types\AuthorType;
+use Tests\data\Types\BirthType;
 use Tests\ModelBasedTest;
 
 class SchemaPathProcessorTest extends ModelBasedTest
@@ -27,12 +33,18 @@ class SchemaPathProcessorTest extends ModelBasedTest
      */
     private $sortMethodFactory;
 
+    /**
+     * @var BirthType
+     */
+    private $birthType;
+
     protected function setUp(): void
     {
         parent::setUp();
         $conditionFactory = new PhpConditionFactory();
         $this->sortMethodFactory = new PhpSortMethodFactory();
         $this->authorType = new AuthorType($conditionFactory);
+        $this->birthType = new BirthType($conditionFactory);
         $typeProvider = new PrefilledTypeProvider([
             $this->authorType,
         ]);
@@ -44,5 +56,33 @@ class SchemaPathProcessorTest extends ModelBasedTest
         $this->expectException(AccessException::class);
         $this->expectExceptionMessage("Access with the path 'foo.bar' into the type class 'Tests\data\Types\AuthorType' was denied because of the path segment 'foo'.");
         $this->schemaPathProcessor->mapSortMethods($this->authorType, $this->sortMethodFactory->propertyAscending('foo', 'bar'));
+    }
+
+    public function testNonSortableType(): void
+    {
+        $this->expectException(AccessException::class);
+        $this->expectExceptionMessage("Methods for sorting were provided but the type class 'Tests\data\Types\BirthType' is not sortable.");
+        $this->schemaPathProcessor->mapSortMethods($this->birthType, $this->sortMethodFactory->propertyAscending('foo', 'bar'));
+    }
+
+    public function testEmptyPath(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Property path must not be empty.');
+
+        $propertyPath = new class(
+            null,
+            '',
+            PropertyPathAccessInterface::UNPACK_RECURSIVE,
+            'foo'
+        ) extends PropertyPath {
+            public function getAsNames(): array
+            {
+                return [];
+            }
+        };
+        $sortMethod = new Ascending(new Property($propertyPath));
+
+        $this->schemaPathProcessor->mapSortMethods($this->authorType, $sortMethod);
     }
 }
