46a1bd4716cebe11fe71d2f762f8f95f5f9655b9
Christian Dressler <dressler@demos-deutschland.de>
2022-09-26 22:16:05 +0200

    feature: implement entity provider in `DoctrineOrmEntityProvider`

--- packages/dql/src/ObjectProviders/DoctrineOrmEntityProvider.php
+++ packages/dql/src/ObjectProviders/DoctrineOrmEntityProvider.php
@@ -10,15 +10,17 @@ use EDT\DqlQuerying\Contracts\ClauseInterface;
 use EDT\DqlQuerying\Contracts\MappingException;
 use EDT\DqlQuerying\Utilities\QueryGenerator;
 use EDT\DqlQuerying\Contracts\OrderByInterface;
+use EDT\Querying\Pagination\OffsetBasedPagination;
 use EDT\Querying\Contracts\ObjectProviderInterface;
 use EDT\Querying\Contracts\SliceException;
-use EDT\Querying\Contracts\SortMethodInterface;
+use EDT\Querying\EntityProviders\OffsetBasedEntityProviderInterface;
 
 /**
  * @template T of object
  * @template-implements ObjectProviderInterface<ClauseInterface, OrderByInterface, T>
+ * @template-implements OffsetBasedEntityProviderInterface<ClauseInterface, OrderByInterface, T>
  */
-class DoctrineOrmEntityProvider implements ObjectProviderInterface
+class DoctrineOrmEntityProvider implements ObjectProviderInterface, OffsetBasedEntityProviderInterface
 {
     /**
      * @var QueryGenerator
@@ -44,6 +46,30 @@ class DoctrineOrmEntityProvider implements ObjectProviderInterface
      */
     public function getObjects(array $conditions, array $sortMethods = [], int $offset = 0, int $limit = null): iterable
     {
+        $queryBuilder = $this->generateQueryBuilder($conditions, $sortMethods, $offset, $limit);
+        return $queryBuilder->getQuery()->getResult();
+    }
+
+    /**
+     * @param list<ClauseInterface>      $conditions
+     * @param list<OrderByInterface>     $sortMethods
+     * @param OffsetBasedPagination|null $pagination
+     *
+     * @return iterable<T>
+     *
+     * @throws MappingException
+     * @throws SliceException
+     */
+    public function getEntities(array $conditions, array $sortMethods, ?object $pagination): iterable
+    {
+        if (null === $pagination) {
+            $offset = 0;
+            $limit = null;
+        } else {
+            $offset = $pagination->getOffset();
+            $limit = $pagination->getLimit();
+        }
+
         $queryBuilder = $this->generateQueryBuilder($conditions, $sortMethods, $offset, $limit);
         return $queryBuilder->getQuery()->getResult();
     }
