f1c1c4912c7a53096db443378c36aaab5e59217c
Christian Dressler <dressler@demos-deutschland.de>
2022-11-10 16:47:22 +0100

    refactor: return type instances by `getSortableProperties`

--- packages/access-definitions/src/Wrapping/Utilities/TypeAccessors/ExternSortableProcessorConfig.php
+++ packages/access-definitions/src/Wrapping/Utilities/TypeAccessors/ExternSortableProcessorConfig.php
@@ -5,24 +5,50 @@ declare(strict_types=1);
 namespace EDT\Wrapping\Utilities\TypeAccessors;
 
 use EDT\Querying\Contracts\PathsBasedInterface;
+use EDT\Wrapping\Contracts\PropertyAccessException;
+use EDT\Wrapping\Contracts\Types\ExposableRelationshipTypeInterface;
 use EDT\Wrapping\Contracts\Types\SortableTypeInterface;
 use EDT\Wrapping\Contracts\Types\TypeInterface;
+use Exception;
 
 /**
  * @template-extends AbstractProcessorConfig<SortableTypeInterface<PathsBasedInterface, PathsBasedInterface, object>>
  */
 class ExternSortableProcessorConfig extends AbstractProcessorConfig
 {
+    public function getPropertyType(TypeInterface $type, string $property): ?TypeInterface
+    {
+        $availableProperties = $type->getSortableProperties();
+        // abort if the (originally accessed/non-de-aliased) property is not available
+        if (!array_key_exists($property, $availableProperties)) {
+            $availablePropertyNames = array_keys($availableProperties);
+            throw PropertyAccessException::propertyNotAvailableInType($property, $type, ...$availablePropertyNames);
+        }
+
+        $targetType = $availableProperties[$property];
+        if (null === $targetType) {
+            return null;
+        }
+
+        if (!$targetType instanceof ExposableRelationshipTypeInterface
+            || !$targetType->isExposedAsRelationship()) {
+            throw PropertyAccessException::notExposedRelationship($targetType);
+        }
+
+        if (!$targetType instanceof SortableTypeInterface){
+            throw PropertyAccessException::typeNotSortable($type);
+        }
+
+        return $targetType;
+    }
+
     public function getProperties(TypeInterface $type): array
     {
-        return $type->getSortableProperties();
+        throw new Exception('Not implemented.');
     }
 
     public function getRelationshipType(string $typeIdentifier): TypeInterface
     {
-        return $this->typeProvider->requestType($typeIdentifier)
-            ->instanceOf(SortableTypeInterface::class)
-            ->exposedAsRelationship()
-            ->getInstanceOrThrow();
+        throw new Exception('Not implemented.');
     }
 }
