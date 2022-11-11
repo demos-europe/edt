cbc6a7bb1bc85db3d0c60d63e978290bae3fd3bf
Christian Dressler <dressler@demos-deutschland.de>
2022-11-11 11:26:42 +0100

    refactor: make types responsible for returned implementations
    When implementing methods like
    `ReadableTypeInterface::getFilterableProperties` the returned
    relationships must implement `ReadableTypeInterface` themselves,
    as otherwise they can not be used to read any properties.
    
    The same goes for `FilterableTypeInterface` and
    `SortableTypeInterface`.
    
    When implementing these three methods the implementing
    type is now also responsible to check
    `ExposableRelationshipTypeInterface::isExposedAsRelationship`
    if so desired. There will be no such checks when using the
    returned relationships.

--- packages/access-definitions/src/Wrapping/Utilities/TypeAccessors/ExternFilterableProcessorConfig.php
+++ packages/access-definitions/src/Wrapping/Utilities/TypeAccessors/ExternFilterableProcessorConfig.php
@@ -5,52 +5,16 @@ declare(strict_types=1);
 namespace EDT\Wrapping\Utilities\TypeAccessors;
 
 use EDT\Querying\Contracts\PathsBasedInterface;
-use EDT\Wrapping\Contracts\PropertyAccessException;
-use EDT\Wrapping\Contracts\RelationshipAccessException;
-use EDT\Wrapping\Contracts\Types\ExposableRelationshipTypeInterface;
 use EDT\Wrapping\Contracts\Types\FilterableTypeInterface;
 use EDT\Wrapping\Contracts\Types\TypeInterface;
-use Exception;
-use function array_key_exists;
 
 /**
  * @template-extends AbstractProcessorConfig<FilterableTypeInterface<PathsBasedInterface, PathsBasedInterface, object>>
  */
 class ExternFilterableProcessorConfig extends AbstractProcessorConfig
 {
-    public function getPropertyType(TypeInterface $type, string $property): ?TypeInterface
-    {
-        $availableProperties = $type->getFilterableProperties();
-        // abort if the (originally accessed/non-de-aliased) property is not available
-        if (!array_key_exists($property, $availableProperties)) {
-            $availablePropertyNames = array_keys($availableProperties);
-            throw PropertyAccessException::propertyNotAvailableInType($property, $type, ...$availablePropertyNames);
-        }
-
-        $targetType = $availableProperties[$property];
-        if (null === $targetType) {
-            return null;
-        }
-
-        if (!$targetType instanceof ExposableRelationshipTypeInterface
-            || !$targetType->isExposedAsRelationship()) {
-            throw RelationshipAccessException::notExposedRelationship($targetType);
-        }
-
-        if (!$targetType instanceof FilterableTypeInterface){
-            throw RelationshipAccessException::typeNotFilterable($type);
-        }
-
-        return $targetType;
-    }
-
     public function getProperties(TypeInterface $type): array
     {
-        throw new Exception('Not implemented.');
-    }
-
-    public function getRelationshipType(string $typeIdentifier): TypeInterface
-    {
-        throw new Exception('Not implemented.');
+        return $type->getFilterableProperties();
     }
 }
